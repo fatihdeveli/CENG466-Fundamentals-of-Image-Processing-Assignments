460d86b4c30ac6a1b4078549a84f0644588ad762
Fatih Develi <fatih.develi@hotmail.com>
2018-12-06 15:37:24 +0300

    Finalized part 2 and part 3

--- the2_part2.m
+++ the2_part2.m
@@ -5,6 +5,7 @@ Fatih Develi 2330892
 
 clear;
 clc;
+warning off;
 
 B1 = imread('the2_input/B1.png');
 B1_height = size(B1, 1);
@@ -39,7 +40,14 @@ filter1 = ifftshift(filter1); % shift back
 B1_t = B1_t .* filter1; % element-wise multiplication
 %figure, imshow(fftshift(abs(B1_t)), [0, 40000]);
 B1_new = ifft2(B1_t);
-%figure,imshow(B1_new, []); %output image
+
+% Transformed values should be mapped.
+B1_new = B1_new/255.0; % Adjust the color range to [0, 1]
+% Map the gray levels to the range [0.0, 1.0]
+B1_new = imadjust(B1_new, [min(B1_new(:)); max(B1_new(:))], [0.0; 1.0]);
+
+%figure, imshow(B3_new); % Display the output image 
+imwrite(B1_new, 'B1_output.png');
 
 
 %%%%% B2 %%%%%
@@ -60,9 +68,15 @@ for x = 1:B2_width
 end
 filter2 = ifftshift(filter2); % shift back
 B2_t = B2_t .* filter2; % element-wise multiplication
-%figure, imshow(fftshift(abs(B2_t)), [0, 5000]);
-B2_new = ifft2(B2_t);
-%figure,imshow(B2_new, []); %output image
+
+B2_new = ifft2(B2_t); % reconstruct image from its transform
+
+% Transformed values should be mapped.
+B2_new = B2_new/255.0; % Adjust the color range to [0, 1]
+% Map the gray levels to the range [0.0, 1.0]
+B2_new = imadjust(B2_new,[min(B2_new(:)); max(B2_new(:))],[0.0; 1.0]);
+
+%figure, imshow(B2_new); % Display the output image 
 imwrite(B2_new, 'B2_output.png');
 
 
@@ -85,10 +99,25 @@ for x = 1:B3_width
 end
 
 filter3 = ifftshift(filter3); % shift back
-%figure, imshow(filter3);
+
 
 % Apply the filter
 B3_t = B3_t .* filter3; % element-wise multiplication
 %figure, imshow(fftshift(abs(B3_t)), [0, 500000]);
 B3_new = ifft2(B3_t);
-%figure,imshow(B3_new, []); %output image
+
+% Transformed values should be mapped.
+for x = 1:B3_width % Adjust the color range to [0.0, 1.0]
+    for y = 1:B3_height
+        B3_new(y,x) = B3_new(y,x)/255.0;
+        if B3_new(y,x) > 1.0
+            B3_new(y,x) = 1.0;
+        end
+    end
+end
+
+% Map the gray levels to the range [0.0, 1.0]
+B3_new = imadjust(B3_new, [min(B3_new(:)); max(B3_new(:))], [0.0; 1.0]);
+
+%figure, imshow(B3_new); % Display the output image
+imwrite(B3_new, 'B3_output.png');
